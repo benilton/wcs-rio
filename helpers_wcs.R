ba9e2aaef4fd8e87a585c1a7502dfb87ca036df5
Benilton Carvalho <benilton@unicamp.br>
2024-08-01 22:03:40 -0300

    adding shell_call

--- helpers_wcs.R
+++ helpers_wcs.R
@@ -1,7 +1,11 @@
 ## helper functions for WCS in Rio
 ## By Benilton Carvalho
 
-
+## https://stackoverflow.com/questions/70025153/how-to-access-the-shell-in-google-colab-when-running-the-r-kernel
+shell_call <- function(command, ...) {
+  result <- system(command, intern = TRUE, ...)
+  cat(paste0(result, collapse = "\n"))
+}
 
 loadPackages = function(pkgs){
   myrequire = function(...){
